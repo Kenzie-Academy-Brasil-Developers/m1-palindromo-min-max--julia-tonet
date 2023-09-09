8a7cf6878ea081b363ba4316e37106b4586aad4b
Julia <juliatonetx@gmail.com>
2023-09-09 20:28:46 +0800

    função isPalindrome adicionada
    Adiciona a função isPalindrome

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  let length = str.length;
+  for (let i = 0; i <= length; i++) {
+    if (str[i] == str[length - 1 - i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
