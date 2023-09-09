de4b119105f1debdcf5ff565f297b7212e895c44
Julia <juliatonetx@gmail.com>
2023-09-09 22:24:02 +0800

    Adicionada arrayMaxMin
    Adicionada a função arrayMaxMin e corrigida a função isPalindrome para que passe em todos os testes.

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function isPalindrome(str) {
   str = str.toLowerCase();
+  str = str.split(" ");
+  str = str.join("");
   let length = str.length;
   for (let i = 0; i <= length; i++) {
-    if (str[i] == str[length - 1 - i]) {
-      return true;
-    } else {
+    if (str[i] !== str[length - 1 - i]) {
       return false;
     }
   }
+  return true;
 }
 
 function arrayMaxMin(arr) {
-  /* Seu código aqui */
+  let arrMax = arr[0];
+  let len = arr.length;
+  let arrMin = arr[len - 1];
+  for (let i = 0; i <= len; i++) {
+    if (arr[i] > arrMax) {
+      arrMax = arr[i];
+    } else {
+      arrMax = arrMax;
+    }
+    if (arr[i] < arrMin) {
+      arrMin = arr[i];
+    } else {
+      arrMin = arrMin;
+    }
+  }
+  let newArr = [arrMin, arrMax];
+  return newArr;
 }
